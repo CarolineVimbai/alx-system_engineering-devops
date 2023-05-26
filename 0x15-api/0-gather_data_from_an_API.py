86223650506bd37fa865330a4182e4fd4c75c92d
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-05-26 17:49:01 +0000

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""using this REST API, for a given employee ID,
-	 returns information about his/her TODO list progress.
-    """
+"""using this REST API, for a given employee ID, 
+returns information about his/her TODO list progress."""
 
 import requests
 from sys import argv
